94dd8b71e98a2ecaea08a90ce38acf09c172432a
abdo-hm02 <abdessamad2002@gmail.com>
2024-06-10 17:20:14 +0200

    setup for project done

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import dotenv from "dotenv";
+
+
+const app = express();
+dotenv.config();
+const port = process.env.PORT || 5000;
+
+
+app.get('/', (req, res) => {
+  res.send('Hello, world!');
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port : ${port}`);
+  });
