901bbbe61b94606a501eea26748ad784aa6e20dc
abdo-hm02 <abdessamad2002@gmail.com>
2024-06-11 18:21:25 +0200

    setting up authentification routes

---
+++ backend/controllers/AuthController.js
@@ -0,0 +1,17 @@
+
+export const signup = async (req,res) => {
+    try {
+        const {fullname,username,password,confirmPassword,gender} = req.body;
+    } catch (error) {
+        
+    }
+};
+
+export const login = (req,res) => {
+    console.log("loginUser")
+};
+
+export const logout = (req,res) => {
+    console.log("logoutUser")
+};
+
--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,14 @@ import express from "express";
 import dotenv from "dotenv";
 
 
+import authRoutes from "./routes/AuthRoutes.js";
+
+
 const app = express();
 dotenv.config();
 const port = process.env.PORT || 5000;
 
+app.use("/api/auth", authRoutes);
 
 app.get('/', (req, res) => {
   res.send('Hello, world!');
